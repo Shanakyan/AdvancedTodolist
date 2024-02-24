24ab7dfe516ca54b0ac453057d79b0a9884a8b21
Narine <96447939+Shanakyan@users.noreply.github.com>
2024-02-24 18:12:14 +0300

    Add files via upload
    Corrected delete by deletion-basket

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ let form = document.querySelector(".form")
 let formInput = document.querySelector("#input")
 let redBtn = document.querySelector("#red-btn");
 let greyBtn = document.querySelector("#grey-btn");
-main = document.querySelector(".list")
-const list = document.querySelector(".list");
+// main = document.querySelector(".list")
+let list = document.querySelector(".list");
 let deleteBtns = document.querySelector(".list__delete-btns")
 let count = 0;  // счетчик созданнных эл. списка.
 
@@ -27,7 +27,7 @@ function createList(str, check){
     label.classList.add("list__label");
     // label.innerText = formInput.value;
 
-    const input = document.createElement("input");
+    let input = document.createElement("input");
     input.setAttribute("type", "checkbox");
     input.setAttribute("id", `checkbox${count}`);
     input.setAttribute("name", `checkbox${count}`);
@@ -47,7 +47,8 @@ function createList(str, check){
     span1.setAttribute("type", "checkbox");
     // span1.classList.add("checkbox");
     span1.classList.add("check_box")
- 
+    //  span1.setAttribute("value","✏️")
+    //  span1.text = "✏️";
     let span2 = document.createElement("span");
     span2.classList.add("delete-item");
     span2.innerText = "🗑";
@@ -55,14 +56,28 @@ function createList(str, check){
    list.append(blockLabel);
    blockLabel.append(label,blockCheckDel);
    label.prepend(input,listText);
-
+//    label.append(span1)
    listText.value = firstUpperLetter(str.toLowerCase());
    blockCheckDel.append(span1,span2);
 
    list.addEventListener("click", changeListItem);
 
+  
+//----удаляет по нажатию кшрзины эл. из списка и локал сторидж-----
+   span2.addEventListener("click",function(e){ 
+    let x = e.target.closest(".field").querySelector(".list_input").value;
+    blockLabel.remove()
+        arr.splice(arr.findIndex(item => item.text == x.toLowerCase()),1)// находим нужный объект по ключу и извлекаем из массива 
+      LS.setItem("task",JSON.stringify(arr)) // запись хранилище  уже без этого эл-а
+    //    console.log(label);
+       label.remove() ;
+   //    del();// удаляет  кнопки и пустой массив 
+   })
+//-------------------------------------------------------------
 
 }
+
+
 form.addEventListener("submit", function(e){
     e.preventDefault();
 
@@ -96,17 +111,16 @@ function changeListItem(e){
      textinput.classList.toggle("out-line");
      textinput.addEventListener("change", function(e){
         arr[str].text=textinput.value;
-        // console.log(x);
-        // console.log(str);  
-        // console.log(arr[arr.findIndex(item => item.text == x.toLowerCase())]);
-        //  arr[arr.findIndex(item => item.text == x.value)] = textinput.value;
-     
+        console.log(x);
+        // console.log(textinput.value);
+        console.log(str);
         LS.setItem("task",JSON.stringify(arr));
         // arr[arr.findIndex(item => item.text === textinput.value)].text = text.value;
      })
- 
+     // находим нужный объект по ключу и извлекаем из массива 
     //  console.log(arr[arr.findIndex(item => item.text === textinput.value)].text);
-  
+    //  console.log(arr);
+    //  LS.setItem("task",JSON.stringify(arr)) // запись хранилище  уже без этого эл-а
     }
 
 }
@@ -115,27 +129,12 @@ redBtn.addEventListener("click", function(e){
     LS.clear()
     arr=[];
     deleteBtns.classList.add("d-none")
-    main.innerText = ''; 
+    list.innerText = ''; 
     formInput.value = '';
 })
 
-list.addEventListener("click", function(e){
-    // console.log(e.target);
-    if(e.target.classList.contains("delete-item"))
- {
 
-    let label = e.target.closest(".list").querySelector("div");
-    let delItem = e.target.closest(".field").querySelector(".list_input")
-     console.log(delItem.value);
-     arr.splice(arr.findIndex(item => item.text === delItem.value.toLowerCase()),1)// находим нужный объект по ключу и извлекаем из массива 
-     console.log(arr);
-     LS.setItem("task",JSON.stringify(arr)) // запись хранилище  уже без этого эл-а
-//     console.log(delItem);
-    label.remove() ;
-}
-//    del();// удаляет  кнопки и пустой массив 
 
-})
 
 if(LS.getItem("task")){ 
     arr =  JSON.parse( LS.getItem("task")) 
@@ -145,7 +144,3 @@ if(LS.getItem("task")){
     }
   
 }
-
-
-
-
